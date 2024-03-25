084dc3ba2e549dfcbae0cf1d5023a952370ee0f4
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-03-25 11:19:11 -0700

    .

--- apps/www/components/signup-form.tsx
+++ apps/www/components/signup-form.tsx
@@ -3,8 +3,8 @@
 import { useState } from "react"
 import { useToast } from "@/components/use-toast"
 
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+import { Button } from "@/components/button"
+import { Input } from "@/components/input"
 
 export const SignupForm = () => {
   const { toast } = useToast()
